abeff5a205aa3cfdb7faaefa8e9dbf942051c0df
Alex Muiruri <content@alexmuiruri.com>
2024-10-21 20:19:36 +0300

    ads

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { RouterProvider } from 'react-router-dom'
-import { routes } from './components/layouts/routes'
-
-const App = () => <RouterProvider router={routes} />
-
-export default App
--- src/components/charts/calendardates.tsx
+++
@@ -1,8 +0,0 @@
-
-const CalendarDates = () => {
-   return <div>
-    
-   </div>
-}
-
-export default CalendarDates
